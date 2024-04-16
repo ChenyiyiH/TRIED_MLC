a1598018f42f0db542b270c34fa8b5863ee6b743
ChenyiyiH <1135080092@qq.com>
2024-04-16 15:34:40 +0800

    update something

--- SIMULATION_NEW_FILE/simout_chbjt_par.m
+++ SIMULATION_NEW_FILE/simout_chbjt_par.m
@@ -15,29 +15,27 @@ frequency_modulate_start = 250;                                                 
 frequency_modulate_end = 1000;                                                                           %调制频率结束点
 frequency_modulate_interval = 50;                                                                        %调制频率间隔                       
 step = (frequency_modulate_end - frequency_modulate_start) / frequency_modulate_interval + 1;            %调制频率步长
-f_out = frequency_modulate_start : frequency_modulate_interval : frequency_modulate_end;                 %输出频率输入量
+f_out = frequency_modulate_start : frequency_modulate_interval : frequency_modulate_end;                 %调制频率输入量
 
 %% 部署并行模型仿真
 tstart = datetime();
 for ii = 1:3
     RUNLEVEL = level(ii);                                                               %确定当前运行模型
     version =strcat('chbjt',RUNLEVEL);
-    model_version = strcat('Par_model_sim_CHBjt',RUNLEVEL);                                 %设置模型版本
+    model_version = strcat('Par_model_sim_CHBjt',RUNLEVEL);                             %设置模型版本
     L_1 = eps;                                                                          %模型内参数                                     
     % in  = repmat(Simulink.SimulationInput,length(f_m));                               %模型输入
     for i = 1:step
         in(i) = Simulink.SimulationInput(model_version);
-        in(i) = in(i).setVariable('f_m',f_out(i));
+        in(i) = in(i).setVariable('f_out',f_out(i));
     end
     out = parsim(in, 'ShowProgress','on','ShowSimulationManager', ...                   %模型仿真
         'off','TransferBaseWorkspaceVariables','on');
+    close_system(model_version);
+    fname = strcat(version,"chbjt",'.mat');                                                 %保存数据
+    save(fname);
+    sprintf('saving file: %s' , fname)
 end
-
-
-close_system(model_version);
-fname = strcat(version,"chbjt",'.mat');                                                 %保存数据
-save(fname);
-sprintf('saving file: %s' , fname)
 tend = datetime();                                                              
 display("Elapsed time:");
 telapsed = tend-tstart;
